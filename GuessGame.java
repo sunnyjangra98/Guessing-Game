083704ff7bcf5b4fb90d3f0915346cc8112448e2
sunnyjangra98 <dushyant16035@iiitd.ac.in>
2017-02-08 19:50:15 +0530

    Add files via upload

---
+++ GuessGame.java
@@ -0,0 +1,69 @@
+package GuessingGame;
+
+public class GuessGame {
+	 Player p1;
+	 Player p2;
+	 //Player p3;      for third player
+	 
+	 public void startGame(){
+		 p1=new Player();
+		 p2=new Player();
+		 //p3=new Player();
+		 
+		 int guessp1=0;
+		 int guessp2=0;
+		 //int guessp3=0;
+		 
+		 boolean p1isRight=false;
+		 boolean p2isRight=false;
+		 //boolean p3isRight=false;
+		 
+		 /*int targetNumber=(int)(Math.random()*10);
+		 System.out.println("Think a number between 0 to 9:");*/
+		 
+		 while(true){
+			 //System.out.println("Number to guess is "+targetNumber);
+			 
+			 int targetNumber=(int)(Math.random()*10);
+			 System.out.println("Think a number between 0 to 9:");
+			 
+			 p1.guess();
+			 p2.guess();
+			 //p3.guess();
+			 
+			guessp1=p1.number;
+			System.out.println("Player 1 guess "+guessp1); 
+			
+			guessp2=p2.number;
+			System.out.println("Player 2 guess "+guessp2); 
+						 
+			 //guessp3=p3.number;
+			 //System.out.println("Player 3 guess "+guessp3);
+			 
+			 if(guessp1==targetNumber){
+				 p1isRight=true;
+			 }
+			 
+			 if(guessp2==targetNumber){
+				 p2isRight=true;
+			 }
+			 
+			 /*if(guessp3==targetNumber){
+				 p3isRight=true;
+			 }*/
+			 
+			 System.out.println("Target number was \""+targetNumber+"\"");
+			 
+			 if(p1isRight || p2isRight){// || p3isRight){
+				 System.out.println("\nWe have a winner!");
+				 System.out.println("Player 1 got it right? "+p1isRight);
+				 System.out.println("Player 2 got it right? "+p2isRight);
+				 //System.out.println("Player 3 got it right? "+p3isRight);
+				 System.out.println("\n\t\t  Game Over.");
+				 break;
+			 }
+			 else
+				 System.out.println("\nPlayers will have to try once again.\n");
+		 }
+	 }
+}
